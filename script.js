e32560f707b69ad60ba3354e73b079f916f1715b
rmkkmr17 <ramkum94@tutanota.com>
2022-09-18 14:50:28 +0200

    Added some styling to the boxes

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const container = document.querySelector(".container");
-numSquares = 4;
+numSquares = 16;
 
 function createDivs(squaresNum) {
     for (let i = 0; i < squaresNum; i++) {
-        let divSquare = document.createElement("div");
-        divSquare.classList.add("div-square");
-        container.appendChild(divSquare);
+        let rows = document.createElement("div");
+        rows.classList.add("div-rows");
+        container.appendChild(rows);
         for (let j = 0; j < squaresNum; j++) {
+            const widthHeight = 560 / squaresNum;
             let divBox = document.createElement("div");
             divBox.classList.add("div-box");
-            divSquare.appendChild(divBox);
+            divBox.style.width = `${widthHeight}px`
+            divBox.style.height = `${widthHeight}px`
+            rows.appendChild(divBox);
         } 
+        
         }
     }
-
 
 createDivs(numSquares);
