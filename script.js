31fffc71c36101ce568604449b018f41e1a5ae28
rmkkmr17 <ramkum94@tutanota.com>
2022-09-18 13:29:19 +0200

    Changed the function to adapt new requirements

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const container = document.querySelector(".container");
+numSquares = 4;
 
-function createDivs(num) {
-    for (let i = 0; i < 256; i++) {
-        const divPixels = document.createElement("div");
-        container.appendChild(divPixels);
+function createDivs(squaresNum) {
+    for (let i = 0; i < squaresNum; i++) {
+        let divSquare = document.createElement("div");
+        divSquare.classList.add("div-square");
+        container.appendChild(divSquare);
+        for (let j = 0; j < squaresNum; j++) {
+            let divBox = document.createElement("div");
+            divBox.classList.add("div-box");
+            divSquare.appendChild(divBox);
+        } 
+        }
     }
-}
 
+
+createDivs(numSquares);
