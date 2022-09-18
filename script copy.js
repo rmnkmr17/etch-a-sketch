b70efaea3115205e3b3b4d720c179b59b3fe6753
rmkkmr17 <ramkum94@tutanota.com>
2022-09-18 19:11:01 +0200

    Added some styling

--- script copy.js
+++ script copy.js
@@ -2,14 +2,7 @@ const container = document.querySelector(".container");
 // let numSquares = 16;
 
 let resetButton = document.querySelector("#btn-reset");
-let btnBlack = document.querySelector("#btn-black");
-btnBlack.addEventListener("click", setBlack);
 
-let btnRGB = document.querySelector("#btn-rgb");
-btnRGB.addEventListener("click", setRGB);
-
-let btnEraser = document.querySelector("#btn-eraser");
-btnEraser.addEventListener("click", eraseDivs)
 function createDivs(squaresNum) {
     const mainContainer = document.createElement("div");
     mainContainer.classList.add("main-container");
@@ -24,11 +17,16 @@ function createDivs(squaresNum) {
             const widthHeight = 560 / squaresNum;
             let divBox = document.createElement("div");
             divBox.classList.add("div-box");
+
+
+            divBox.addEventListener("mouseenter", () => {
+                divBox.style.backgroundColor = "black";
+            })
             divBox.style.width = `${widthHeight}px`
             divBox.style.height = `${widthHeight}px`
             rows.appendChild(divBox);
 
-
+            
         }
     }
 
@@ -36,36 +34,8 @@ function createDivs(squaresNum) {
 
 createDivs(16);
 
-function setBlack() {
-    let divBoxes = document.querySelectorAll(".div-box");
-    divBoxes.forEach((div) => {
-        div.addEventListener("mousedown", () =>{
-            div.style.backgroundColor = "black";
-        })
-    })
-}
-
-function setRGB() {
-    let divBoxes = document.querySelectorAll(".div-box");
-    divBoxes.forEach((div) => {
-        div.addEventListener("mouseenter", () =>{
-            div.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-        })
-    })
-}
-
-function eraseDivs() {
-    let divBoxes = document.querySelectorAll(".div-box");
-    divBoxes.forEach((div) => {
-        div.addEventListener("mouseenter", () =>{
-            div.style.backgroundColor = "white";
-        })
-    })
-}
-
-
 resetButton.addEventListener("click", () => {
-
+    
     let input = Number(prompt("How many squares per side do you want?", "Min 1 = Max 100"));
     if (input > 100 || input < 1) {
         alert("You picked an invalid number.");
@@ -74,8 +44,6 @@ resetButton.addEventListener("click", () => {
         const mainContainer = document.querySelector(".main-container");
         mainContainer.remove();
         createDivs(input);
-        setBlack();
-        setRGB();
     }
 })
 
