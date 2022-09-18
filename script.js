f080e59a844d0ae187f36b4350b532788f0e94fd
rmkkmr17 <ramkum94@tutanota.com>
2022-09-18 18:04:18 +0200

    reset

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ const container = document.querySelector(".container");
 
 let resetButton = document.querySelector("#btn-reset");
 
+
+
 function createDivs(squaresNum) {
 
     const mainContainer = document.createElement("div");
     mainContainer.classList.add("main-container");
+    container.appendChild(mainContainer);
 
     for (let i = 0; i < squaresNum; i++) {
         let rows = document.createElement("div");
         rows.classList.add("div-rows");
         mainContainer.appendChild(rows);
+
         for (let j = 0; j < squaresNum; j++) {
             const widthHeight = 560 / squaresNum;
             let divBox = document.createElement("div");
@@ -19,26 +23,37 @@ function createDivs(squaresNum) {
             divBox.style.width = `${widthHeight}px`
             divBox.style.height = `${widthHeight}px`
             rows.appendChild(divBox);
-            divBox.addEventListener("mouseenter", () =>{
-                divBox.style.backgroundColor = "black";
-            })
-        } 
-        
+
+            
         }
-        container.appendChild(mainContainer);
     }
+
+}
 
 createDivs(16);
 
+
+
+
+
+
+
 resetButton.addEventListener("click", () => {
-    let input = Number(prompt("How many squares per side do you want?"));
+    
+    let input = Number(prompt("How many squares per side do you want?", "Min 1 = Max 100"));
     if (input > 100 || input < 1) {
-    alert("You picked an invalid number.");
+        alert("You picked an invalid number.");
     }
     else {
         const mainContainer = document.querySelector(".main-container");
         mainContainer.remove();
-        createDivs(input);  
+        createDivs(input);
     }
 })
 
+const divBoxes = document.querySelectorAll(".div-box");
+divBoxes.forEach((div) => {
+    div.addEventListener("mouseenter", () => {
+        div.style.backgroundColor = "black";
+    }) 
+})
